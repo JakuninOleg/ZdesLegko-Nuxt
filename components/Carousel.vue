bcd6158879e1369244fa81e90b938caf78e32059
olegjakunin <oleg.kemz@gmail.com>
2019-12-06 17:42:29 +0300

    init commit

---
+++ components/Carousel.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <vueper-slides>
+      <vueper-slide v-for="i in 5" :key="i" :title="i.toString()" class="slider" />
+    </vueper-slides>
+  </div>
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+export default {
+  components: {
+    VueperSlides,
+    VueperSlide
+  }
+}
+</script>
